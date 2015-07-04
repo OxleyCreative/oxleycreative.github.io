8a04db45d8c7fc079ced4f908514c08944935b8e
Rob <projectx118@hotmail.com>
2015-07-04 18:44:04 -0400

    Redesign Work

--- scripts/style.js
+++ scripts/style.js
@@ -1,105 +1,24 @@
-$(document).ready(function()
-{
-
-  $("#sectionweb ul").hide();
-  $("#sectionposter ul").hide();
-  $("#sectionidentity ul").hide();
-  $("#sectioneditorial ul").hide();
-  $("#sectionmiscellaneous ul").hide();
-  $("#webcontent").hide();
-  $("#postercontent").hide();
-  $("#identitycontent").hide();
-  $("#editorialcontent").hide();
-  $("#misccontent").hide();
-  
-  $("#blogtopic1 ul").hide();
-  $("#blogtopic2 ul").hide();
-  $("#blogtopic3 ul").hide();
-  $("#blogtopic4 ul").hide();
-  $("#blogtopic1 h2").click(function()
-  {
-    $("#blogtopic1 ul").slideToggle(600);
+$(document).on('ready', function() {
+  $('.workHeader').on('click', function() {
+    if ($(this).parent().hasClass('expanded')) {
+      $(this).parent().removeClass('expanded');
+      $(this).parent().find($('.workContent')).slideUp(3000);
+    } else {
+      $('.workBlock').removeClass('expanded');
+      $('.workContent').slideUp(3000);
+      $(this).parent().addClass('expanded');
+      $(this).parent().find($('.workContent')).slideDown(3000);
+    }
   });
-  $("#blogtopic2 h2").click(function()
-  {
-    $("#blogtopic2 ul").slideToggle(600);
+  $('.workImg a').on('click', function(event) {
+    event.preventDefault();
+    var imgPath = $(this).attr('href');
+    var imgCaption = $(this).attr('alt');
+    $('.overlay').css('background-image','url(' + imgPath + ')');
+    $('.caption').html(imgCaption);    
+    $('.overlay').addClass('show');
   });
-  $("#blogtopic3 h2").click(function()
-  {
-    $("#blogtopic3 ul").slideToggle(600);
-  });
-  $("#blogtopic4 h2").click(function()
-  {
-    $("#blogtopic4 ul").slideToggle(600);
-  });
-  
-  $("#sectionweb h2").click(function()
-  {
-  $("#sectionposter ul").hide();
-  $("#sectionidentity ul").hide();
-  $("#sectioneditorial ul").hide();
-  $("#sectionmiscellaneous ul").hide();
-  $("#postercontent").hide();
-  $("#identitycontent").hide();
-  $("#editorialcontent").hide();
-  $("#misccontent").hide();
-    $("#sectionweb ul").slideToggle(600);
-	$("#webcontent").slideToggle(1500);
-  });
-  
-  $("#sectionposter h2").click(function()
-  {
-  $("#sectionweb ul").hide();
-  $("#sectionidentity ul").hide();
-  $("#sectioneditorial ul").hide();
-  $("#sectionmiscellaneous ul").hide();
-  $("#webcontent").hide();
-  $("#identitycontent").hide();
-  $("#editorialcontent").hide();
-  $("#misccontent").hide();
-    $("#sectionposter ul").slideToggle(600);
-	$("#postercontent").slideToggle(1500);
-  });
-  
-  $("#sectionidentity h2").click(function()
-  {
-  $("#sectionweb ul").hide();
-  $("#sectionposter ul").hide();
-  $("#sectioneditorial ul").hide();
-  $("#sectionmiscellaneous ul").hide();
-  $("#webcontent").hide();
-  $("#postercontent").hide();
-  $("#editorialcontent").hide();
-  $("#misccontent").hide();
-    $("#sectionidentity ul").slideToggle(600);
-	$("#identitycontent").slideToggle(1500);
-  });
-  
-   $("#sectioneditorial h2").click(function()
-  {
-  $("#sectionweb ul").hide();
-  $("#sectionposter ul").hide();
-  $("#sectionidentity ul").hide();
-  $("#sectionmiscellaneous ul").hide();
-  $("#webcontent").hide();
-  $("#postercontent").hide();
-  $("#identitycontent").hide();
-  $("#misccontent").hide();
-    $("#sectioneditorial ul").slideToggle(600);
-	$("#editorialcontent").slideToggle(1500);
-  });
-  
-     $("#sectionmiscellaneous h2").click(function()
-  {
-  $("#sectionweb ul").hide();
-  $("#sectionposter ul").hide();
-  $("#sectionidentity ul").hide();
-  $("#sectioneditorial ul").hide();
-  $("#webcontent").hide();
-  $("#postercontent").hide();
-  $("#identitycontent").hide();
-  $("#editorialcontent").hide();
-    $("#sectionmiscellaneous ul").slideToggle(600);
-	$("#misccontent").slideToggle(1500);
+  $('.close').on('click', function() {
+    $('.overlay').removeClass('show');
   });
 });
