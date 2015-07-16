66402c128a570e7048699ff3ae93a2d4e26c6603
Rob <projectx118@hotmail.com>
2015-07-15 22:41:41 -0400

    Content Added.

--- scripts/style.js
+++ scripts/style.js
@@ -1,15 +1,15 @@
 $(document).on('ready', function() {
-  $('.workHeader').on('click', function() {
-    if ($(this).parent().hasClass('expanded')) {
-      $(this).parent().removeClass('expanded');
-      $(this).parent().find($('.workContent')).slideUp(3000);
-    } else {
-      $('.workBlock').removeClass('expanded');
-      $('.workContent').slideUp(3000);
-      $(this).parent().addClass('expanded');
-      $(this).parent().find($('.workContent')).slideDown(3000);
-    }
-  });
+//  $('.workHeader').on('click', function() {
+//    if ($(this).parent().hasClass('expanded')) {
+//      $(this).parent().removeClass('expanded');
+//      $(this).parent().find($('.workContent')).slideUp(3000);
+//    } else {
+//      $('.workBlock').removeClass('expanded');
+//      $('.workContent').slideUp(3000);
+//      $(this).parent().addClass('expanded');
+//      $(this).parent().find($('.workContent')).slideDown(3000);
+//    }
+//  });
   $('.workImg a').on('click', function(event) {
     event.preventDefault();
     var imgPath = $(this).attr('href');
