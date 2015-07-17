bbea40eb3a91d720069cad91307892bf5f7654b7
Rob <projectx118@hotmail.com>
2015-07-16 22:40:11 -0400

    Modal Updates.

--- scripts/style.js
+++ scripts/style.js
@@ -10,15 +10,22 @@ $(document).on('ready', function() {
 //      $(this).parent().find($('.workContent')).slideDown(3000);
 //    }
 //  });
+var closedModalHashStateId = "#modalClosed";
+var openModalHashStateId = "#modalOpen";
+window.location.hash = closedModalHashStateId;
+
   $('.workImg a').on('click', function(event) {
     event.preventDefault();
     var imgPath = $(this).attr('href');
     var imgCaption = $(this).attr('alt');
     $('.overlay').css('background-image','url(' + imgPath + ')');
-    $('.caption').html(imgCaption);    
+    $('.caption').html(imgCaption + ' ( <a href="' + imgPath + '">Full Size</a> )');    
     $('.overlay').addClass('show');
+	window.location.hash = openModalHashStateId;
   });
   $('.close').on('click', function() {
     $('.overlay').removeClass('show');
+	window.history.back();
   });
+  window.onhashchange = function () { if(window.location.hash != "#modalOpen") { $('.overlay').removeClass('show'); } }
 });
